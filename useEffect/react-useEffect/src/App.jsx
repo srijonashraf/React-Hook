46fe41f505f96c4c74a2fb284b0bcb19938eb722
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2023-12-07 14:56:39 +0600

    toast message applied for view button

--- useEffect/react-useEffect/src/App.jsx
+++ useEffect/react-useEffect/src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
     if (!show) {
       toast.success("Source Code view expanded");
     } else {
-      toast.error("Source view hidden");
+      toast.error("Source Code view hidden");
     }
   };
 
